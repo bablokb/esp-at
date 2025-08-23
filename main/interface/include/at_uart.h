bc6affdd14188942b40068b35bbdb7b45c81dc42
Bernhard Bablok <bablokb@gmx.de>
2025-08-23 16:14:16 +0200

    optimize UART throughput

--- main/interface/include/at_uart.h
+++ main/interface/include/at_uart.h
@@ -43,9 +43,9 @@
 #define AT_UART_TX_BUFFER_SIZE                      2048                    /**< uart tx buffer size */
 #define AT_UART_QUEUE_SIZE                          15                      /**< uart queue size */
 #else
-#define AT_UART_RX_BUFFER_SIZE                      2048                    /**< uart rx buffer size */
-#define AT_UART_TX_BUFFER_SIZE                      8192                    /**< uart tx buffer size */
-#define AT_UART_QUEUE_SIZE                          30                      /**< uart queue size */
+#define AT_UART_RX_BUFFER_SIZE                      16384                   /**< uart rx buffer size */
+#define AT_UART_TX_BUFFER_SIZE                      16384                   /**< uart tx buffer size */
+#define AT_UART_QUEUE_SIZE                          100                     /**< uart queue size */
 #endif
 
 /**
